5b1185d313a913e17faef358e6a3c537bad01d03
Unknown <bf1081@nyu.edu>
2017-01-30 11:04:37 -0500

    Added the project

---
+++ Dynamic Torch Light/Assets/Player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 3; // speed in meters per second
+	bool jumping;
+	public float jumpSpd;
+	private float xpos;
+	float curJumpVel;
+	float startY;
+	public float gravity;
+	// Use this for initialization
+	void Start () {
+		startY = transform.position.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 Dir = Vector3.zero;
+		Dir.z = Input.GetAxis("Horizontal"); // get result of AD keys in X
+		//Dir.z = Input.GetAxis("Vertical"); // get result of WS keys in Z
+		// move this object at frame rate independent speed:
+		transform.position += Dir * speed * Time.deltaTime;
+
+	
+	
+		DealWithInput ();
+		if (jumping) {
+			DealWithJumping ();
+		}
+	
+
+	}
+
+	void DealWithInput () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (jumping) {
+				//Throwball ();
+			} else {
+				Jump ();
+			}
+		}
+	}
+
+	void Jump() {
+		jumping = true;
+		curJumpVel = jumpSpd;
+	}
+
+	void DealWithJumping() {
+		transform.position += new Vector3 (0, curJumpVel, 0);
+		curJumpVel -= gravity;
+		if (transform.position.y <= startY) {
+			jumping = false;
+			transform.position = new Vector3 (transform.position.x, startY, 0);
+		}
+	}
+}
