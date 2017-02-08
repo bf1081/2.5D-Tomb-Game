fb38da563b36c076dd551e321b164f8598f9be35
Unknown <bf1081@nyu.edu>
2017-02-08 12:17:58 -0500

    Level work

---
+++ Tocoya/Assets/Scripts/LevelBuilder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBuilder : MonoBehaviour {
+	public GameObject StraightAcrossPath;
+	public Transform PathMaker;
+	private int path = 5;
+	float path2 = 5;
+	// Use this for initialization
+	void Start () {
+		int path = Random.Range (0, 50);
+		Debug.Log (path2);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < path2; i++) {
+			Instantiate (StraightAcrossPath, PathMaker.position, PathMaker.rotation);
+		}
+	}
+}
